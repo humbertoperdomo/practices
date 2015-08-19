71292e5bb55924709b2fb228a92f78ff1c9223b1
Humberto Perdomo Alvarez <humbertoperdomo@users.noreply.github.com>
2015-08-18 19:28:23 -0500

    Update App.java

--- java/Threads/JavaMultithreading/part08/src/WaitAndNotify/App.java
+++ java/Threads/JavaMultithreading/part08/src/WaitAndNotify/App.java
@@ -1,8 +1,33 @@
 package WaitAndNotify;
 
 public class App {
-  public static void main(String[] args) {
-
+  public static void main(String[] args) throws InterruptedException {
+    Processor processor = new Processor();
+    
+    Thread t1 = new Thread (new Runnable() {
+      @Override
+      public void run() {
+        try {
+          processor.produce();
+        } catch (InterruptedException ie) {
+          ie.printStackTrace();
+        }
+      }
+    });
+    
+    Thread t2 = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          processor.comsumer();
+        } catch (InterruptedException ie) {
+          ie.printStackTrace()
+        }
+      }
+    });
+    
+    t1.start();
+    t2.start();
   }
 }
 
